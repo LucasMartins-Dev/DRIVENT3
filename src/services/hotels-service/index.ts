3efecd1d4dcd38b6ab60d9ad9e62464f66654054
lucasmartins-dev <lucasmartinsms.dev@gmail.com>
2023-04-24 15:43:00 -0300

    feat: hotels services

---
+++ src/services/hotels-service/index.ts
@@ -0,0 +1,34 @@
+import { Hotel } from '@prisma/client';
+import { notFoundError, paymentRequired } from '@/errors';
+import hotelsRepository from '@/repositories/hotels-repository';
+import ticketsRepository from '@/repositories/tickets-repository';
+import { HotelById } from '@/protocols';
+
+async function checkConditions(userId: number): Promise<void> {
+  const enrollmentId = await ticketsRepository.findEnrollmentByUserId(userId);
+  if (!enrollmentId) throw notFoundError();
+  const ticket = await ticketsRepository.getTicketByIds(0, enrollmentId.id);
+  if (!ticket) throw notFoundError();
+  if (ticket.status !== 'PAID' || ticket.TicketType.isRemote || !ticket.TicketType.includesHotel)
+    throw paymentRequired();
+}
+
+async function getHotels(): Promise<Hotel[]> {
+  const hotels: Hotel[] = await hotelsRepository.getHotels();
+  if (!hotels.length) throw notFoundError();
+  return hotels;
+}
+
+async function getHotelsById(hotelId: number): Promise<HotelById> {
+  const hotel: HotelById = await hotelsRepository.getHotelById(hotelId);
+  if (!hotel) throw notFoundError();
+  return hotel;
+}
+
+const hotelsService = {
+  checkConditions,
+  getHotels,
+  getHotelsById,
+};
+
+export default hotelsService;
