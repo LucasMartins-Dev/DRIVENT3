b6106076389b1d0edef1b6c256d46772af268491
lucasmartins-dev <lucasmartinsms.dev@gmail.com>
2023-04-24 15:39:31 -0300

    feat: hotels controller

---
+++ src/controllers/hotels-controller.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Response } from 'express';
+import httpStatus from 'http-status';
+import { Hotel } from '@prisma/client';
+import { AuthenticatedRequest } from '@/middlewares';
+import hotelsService from '@/services/hotels-service';
+
+export async function getHotels(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  const { userId } = req;
+  try {
+    await hotelsService.checkConditions(userId);
+    const hotels: Hotel[] = await hotelsService.getHotels();
+    return res.status(httpStatus.OK).send(hotels);
+  } catch (error) {
+    if (error.name !== 'NotFoundError' && error.name !== 'PaymentRequired') {
+      return res.sendStatus(httpStatus.BAD_REQUEST);
+    }
+    next(error);
+  }
+}
+
+export async function getHotelsById(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  const { userId } = req;
+  const { hotelId } = req.params;
+  try {
+    await hotelsService.checkConditions(userId);
+    const hotel: Hotel = await hotelsService.getHotelsById(Number(hotelId));
+    res.status(httpStatus.OK).send(hotel);
+  } catch (error) {
+    if (error.name !== 'NotFoundError' && error.name !== 'PaymentRequired') {
+      return res.sendStatus(httpStatus.BAD_REQUEST);
+    }
+    next(error);
+  }
+}
